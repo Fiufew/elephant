a92b8567102521b352b48e3569fa01f3176f4471
sunar87 <pankych@mail.ru>
2024-11-01 22:07:14 +0300

    first commit

---
+++ backend/admin.py
@@ -0,0 +1,63 @@
+from django.contrib import admin
+from .models import Category, Color, Brand, Model, Car, Price, Bid, Investor
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Investor)
+class InvestorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Model)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'is_booked', 'created_at', 'updated_at')
+    list_filter = ('brand', 'model', 'is_booked')
+    search_fields = ('brand__name', 'model__name', 'state_number')
+    prepopulated_fields = {'slug': ('brand', 'model')}
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('car_price', 'season_one',
+                    'season_two', 'season_three',
+                    'season_four')
+
+
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('car', 'price', 'pickup_location',
+                    'dropoff_location', 'pickup_time', 'dropoff_time',
+                    'renter_first_name', 'renter_last_name',
+                    'renter_birthdate', 'renter_phone', 'renter_email',
+                    'contact_method', 'comment', 'bid_preparer')
+    list_filter = ('car', 'price', 'contact_method')
+    search_fields = ('renter_first_name', 'renter_last_name',
+                     'renter_email', 'bid_preparer')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "price":
+            kwargs["queryset"] = Price.objects.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
---
+++ backend/models.py
@@ -0,0 +1,173 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'Color'
+        verbose_name_plural = 'Colors'
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'Brand'
+        verbose_name_plural = 'Brands'
+
+    def __str__(self):
+        return self.name
+
+
+class Investor(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'Investor'
+        verbose_name_plural = 'Investors'
+
+    def __str__(self):
+        return self.name
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=255)
+    brand = models.ForeignKey(Brand,
+                              related_name='brands',
+                              on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+        verbose_name = 'Model'
+        verbose_name_plural = 'Models'
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='categories',
+                                 on_delete=models.CASCADE)
+    color = models.ForeignKey(Color,
+                              related_name='colors',
+                              on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand,
+                              related_name='brands_in_car',
+                              on_delete=models.CASCADE)
+    model = models.ForeignKey(Model,
+                              related_name='models_in_car',
+                              on_delete=models.CASCADE)
+    investor = models.ForeignKey(Investor,
+                                 related_name='investors',
+                                 on_delete=models.CASCADE)
+    is_booked = models.BooleanField(default=False)
+    insurance = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    state_number = models.CharField(max_length=63)
+    slug = models.SlugField(unique=True, max_length=127)
+    photo = models.ImageField(blank=True, upload_to='cars/%Y/%m/%d')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['brand']
+        indexes = [
+            models.Index(fields=['brand']),
+            models.Index(fields=['-created_at']),
+        ]
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
+
+    def __str__(self):
+        return f'{self.brand}: {self.model}'
+
+
+class Price(models.Model):
+    car_price = models.OneToOneField(Car,
+                                     on_delete=models.CASCADE,
+                                     related_name='price')
+    season_one = models.DecimalField(max_digits=10, decimal_places=2)
+    season_two = models.DecimalField(max_digits=10, decimal_places=2)
+    season_three = models.DecimalField(max_digits=10, decimal_places=2)
+    season_four = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Price'
+        verbose_name_plural = 'Prices'
+
+    def __str__(self):
+        return 'Актуальная цена'
+
+
+class Bid(models.Model):
+    CONTACT_CHOICES = [
+        ('telegram', 'Telegram'),
+        ('whatsapp', 'WhatsApp'),
+        ('viber', 'Viber'),
+    ]
+    car = models.ForeignKey(Car, related_name='booking_requests',
+                            on_delete=models.CASCADE)
+    price = models.ForeignKey(Price, related_name='booking_requests_price',
+                              on_delete=models.CASCADE)
+    pickup_location = models.CharField(max_length=511)
+    dropoff_location = models.CharField(max_length=511)
+    pickup_time = models.DateTimeField()
+    dropoff_time = models.DateTimeField()
+    renter_first_name = models.CharField(max_length=255)
+    renter_middle_name = models.CharField(max_length=255,
+                                          blank=True,
+                                          null=True)
+    renter_last_name = models.CharField(max_length=255)
+    renter_birthdate = models.DateField()
+    renter_phone = models.CharField(max_length=20)
+    renter_email = models.EmailField()
+    contact_method = models.CharField(max_length=10, choices=CONTACT_CHOICES)
+    comment = models.TextField(blank=True, null=True)
+    bid_preparer = models.CharField(max_length=127)
+
+    class Meta:
+        verbose_name = 'Bid'
+        verbose_name_plural = 'Bids'
+
+    def __str__(self):
+        return f"Bid for {self.car.brand.name} {self.car.model.name} by {self.renter_first_name} {self.renter_last_name}"
+
