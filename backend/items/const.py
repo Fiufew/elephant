00229329d4bf1e9e76406041fa259bfc835bc61f
Fiufew <205203eric@gmail.com>
2025-03-18 23:02:35 +0500

    change letter in consts

--- backend/items/const.py
+++ backend/items/const.py
@@ -1,63 +1,63 @@
 MAX_PROBLEM_LEN = 512
 
 CURRENCY_CHOICES = [
-    ('thb', 'THB'),
-    ('usd', 'USD'),
-    ('rub', 'RUB'),
+    ('thb', 'thb'),
+    ('usd', 'usd'),
+    ('rub', 'rub'),
 ]
 
 FUEL_CHOICES = [
-    ('Benzin', 'Benzin'),
-    ('Dizel', 'Dizel'),
-    ('Hybrid', 'Hybrid'),
-    ('Turbo Dizel', 'Turbo Dizel'),
-    ('Gaz', 'Gaz'),
-    ('Electricity', 'Electricity'),
+    ('Benzin', 'benzin'),
+    ('Dizel', 'dizel'),
+    ('Hybrid', 'hybrid'),
+    ('Turbo Dizel', 'turbo dizel'),
+    ('Gaz', 'gaz'),
+    ('Electricity', 'electricity'),
 ]
 
 TRANSMISSION_CHOICES = [
-    ('Manual', 'Manual'),
-    ('Automatic', 'Automatic'),
-    ('Automatic + Manual', 'Automatic + Manual'),
+    ('Manual', 'manual'),
+    ('Automatic', 'automatic'),
+    ('Automatic + Manual', 'automatic + manual'),
 ]
 
 DRIVE_CHOICES = [
-    ('Front wheel', 'Front wheel'),
-    ('Rear wheel', 'Rear wheel'),
+    ('Front wheel', 'front wheel'),
+    ('Rear wheel', 'rear wheel'),
     ('4 wheels', '4 wheels'),
 ]
 
 CATEGORY_DRIVES_LICENSE_CHOICES = [
-    ('A', 'A'),
-    ('A1', 'A1'),
-    ('A or B or M', 'A or B or M'),
-    ('B', 'B'),
-    ('B1', 'B1'),
-    ('BE', 'BE'),
-    ('M', 'M'),
+    ('A', 'a'),
+    ('A1', 'a1'),
+    ('A or B or M', 'a or b or m'),
+    ('B', 'b'),
+    ('B1', 'b1'),
+    ('BE', 'be'),
+    ('M', 'm'),
 ]
 
 AIR_CONDITIONER_CHOICES = [
-    ('Air conditioning', 'Air conditioning'),
+    ('Air conditioning', 'air conditioning'),
     ('1-zone climate control', '1-zone climate control'),
     ('2-zone climate control', '2-zone climate control'),
     ('4-zone climate control', '4-zone climate control'),
-    ('None', 'None'),
+    ('None', 'none'),
 ]
 
 INTERIOR_CHOICES = [
-    ('Fabric', 'Fabric'),
-    ('Leather', 'Leather'),
-    ('Sport', 'Sport'),
-    ('Sport Leather', 'Sport Leather'),
+    ('Fabric', 'fabric'),
+    ('Leather', 'leather'),
+    ('Sport', 'sport'),
+    ('Sport Leather', 'sport leather'),
 ]
 
 ROOF_CHOICES = [
-    ('Standard', 'Standard'),
-    ('Sunroof', 'Sunroof'),
-    ('Rigid foldable automatic', 'Rigid foldable automatic'),
-    ('Soft foldable manual', 'Soft foldable manual'),
-    ('Soft foldable automatic', 'Soft foldable automatic'),
+    ('Standard', 'standard'),
+    ('Sunroof', 'sunroof'),
+    ('Rigid foldable automatic', 'rigid foldable automatic'),
+    ('Soft foldable manual', 'soft foldable manual'),
+    ('Soft foldable automatic', 'soft foldable automatic'),
 ]
 
 POWERED_WINDOW_CHOICES = [
@@ -66,45 +66,45 @@ POWERED_WINDOW_CHOICES = [
 ]
 
 SIDE_WHEEL_CHOICES = [
-    ('Left', 'Left'),
-    ('Right', 'Right'),
+    ('Left', 'left'),
+    ('Right', 'right'),
 ]
 
 COLOR_CHOICES = [
-    ('White', 'White'),
-    ('Black', 'Black'),
-    ('Grey', 'Grey'),
-    ('Red', 'Red'),
-    ('Blue', 'Blue'),
-    ('Green', 'Green'),
-    ('Yellow', 'Yellow'),
-    ('Brown', 'Brown'),
-    ('Beige', 'Beige'),
-    ('Orange', 'Orange'),
-    ('Silver', 'Silver'),
-    ('Sky blue', 'Sky blue'),
-    ('Purple', 'Purple'),
+    ('White', 'white'),
+    ('Black', 'black'),
+    ('Grey', 'grey'),
+    ('Red', 'red'),
+    ('Blue', 'blue'),
+    ('Green', 'green'),
+    ('Yellow', 'yellow'),
+    ('Brown', 'brown'),
+    ('Beige', 'beige'),
+    ('Orange', 'orange'),
+    ('Silver', 'silver'),
+    ('Sky blue', 'sky blue'),
+    ('Purple', 'purple'),
 ]
 
 BODY_TYPE_CHOICES = [
-    ('Sedan', 'Sedan'),
-    ('Hatchback', 'Hatchback'),
-    ('Wagon', 'Wagon'),
-    ('Minivan', 'Minivan'),
-    ('Minibus', 'Minibus'),
-    ('Crossover', 'Crossover'),
-    ('Pickup', 'Pickup'),
-    ('Convertible', 'Convertible'),
-    ('Scooter', 'Scooter'),
-    ('Motorcycle', 'Motorcycle'),
-    ('ATV', 'ATV'),
-    ('Buggy', 'Buggy'),
-    ('Coupe', 'Coupe'),
+    ('Sedan', 'sedan'),
+    ('Hatchback', 'hatchback'),
+    ('Wagon', 'wagon'),
+    ('Minivan', 'minivan'),
+    ('Minibus', 'minibus'),
+    ('Crossover', 'crossover'),
+    ('Pickup', 'pickup'),
+    ('Convertible', 'convertible'),
+    ('Scooter', 'scooter'),
+    ('Motorcycle', 'motorcycle'),
+    ('ATV', 'atv'),
+    ('Buggy', 'buggy'),
+    ('Coupe', 'coupe'),
 ]
 
 AGGREGATOR_CHOICES = [
-    ('Localrent', 'Localrent'),
-    ('Trip', 'Trip'),
-    ('Klook', 'Klook'),
-    ('Rentconnected', 'Rentconnected'),
+    ('Localrent', 'localrent'),
+    ('Trip', 'trip'),
+    ('Klook', 'klook'),
+    ('Rentconnected', 'rentconnected'),
 ]
