03c9cbb8fd6e9fc4c94b652b5e64d071d1c3f565
Fiufew <205203eric@gmail.com>
2025-03-21 23:08:34 +0500

    jwt add

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from .views import (
     BrandViewSet, CarModelViewSet, ProblemViewSet,
@@ -30,8 +35,13 @@ router.register(
     r'applications', ApplicationViewSet, basename='application')
 router.register(r'misc', MiscViewSet, basename='misc')
 
+
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path(
+        'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        'api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
