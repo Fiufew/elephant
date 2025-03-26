566a22033414892cd13ad8c997825e1fb7a0c5a8
Erik <205203eric@gmail.com>
2025-03-26 15:14:05 +0500

    change models

--- backend/items/models.py
+++ backend/items/models.py
@@ -200,13 +200,13 @@ class Other(models.Model):
 
 
 class ACT(models.Model):
-    number = models.CharField(  
+    name = models.CharField(  # название страховки
         max_length=128
     )
-    is_expired = models.BooleanField(
+    is_expired = models.BooleanField(  # истекла ли, false, значит нет, true - да
         default=False
     )
-    expired_at = models.DateTimeField(
+    expired_at = models.DateTimeField(  # страховка действительна до этой даты
         null=True,
         blank=True
     )
@@ -216,13 +216,13 @@ class ACT(models.Model):
 
 
 class FirstClass(models.Model):
-    number = models.CharField(
+    name = models.CharField(  # название страховки
         max_length=128
     )
-    is_expired = models.BooleanField(
+    is_expired = models.BooleanField(  # истекла ли, false, значит нет, true - да
         default=False
     )
-    expired_at = models.DateTimeField(
+    expired_at = models.DateTimeField(  # страховка действительна до этой даты
         null=True,
         blank=True
     )
@@ -232,13 +232,13 @@ class FirstClass(models.Model):
 
 
 class SecondClass(models.Model):
-    number = models.CharField(
+    name = models.CharField(  # название страховки
         max_length=128
     )
-    is_expired = models.BooleanField(
+    is_expired = models.BooleanField(  # истекла ли, false, значит нет, true - да
         default=False
     )
-    expired_at = models.DateTimeField(
+    expired_at = models.DateTimeField(  # страховка действительна до этой даты
         null=True,
         blank=True
     )
@@ -248,13 +248,13 @@ class SecondClass(models.Model):
 
 
 class Tax(models.Model):
-    number = models.CharField(
+    name = models.CharField(  # название страховки
         max_length=128
     )
-    is_expired = models.BooleanField(
+    is_expired = models.BooleanField(  # истекла ли, false, значит нет, true - да
         default=False
     )
-    expired_at = models.DateTimeField(
+    expired_at = models.DateTimeField(  # страховка действительна до этой даты
         null=True,
         blank=True
     )
@@ -263,8 +263,24 @@ class Tax(models.Model):
         return self.number
 
 
+class Bluebook(models.Model):
+    name = models.CharField(  # название страховки
+        max_length=128
+    )
+    is_expired = models.BooleanField(  # истекла ли, false, значит нет, true - да
+        default=False
+    )
+    expired_at = models.DateTimeField(  # страховка действительна до этой даты
+        null=True,
+        blank=True
+    )
+    # добавить фото
+    def __str__(self):
+        return self.number
+
+
 class Photo(models.Model):
-    car_image = models.ImageField(
+    car_image = models.ImageField(  # фотография авто
         null=True,
         blank=True,
         upload_to="files/documents_and_other/"
@@ -272,101 +288,107 @@ class Photo(models.Model):
 
 
 class Car(models.Model):
-    brand = models.ForeignKey(
+    brand = models.ForeignKey(  # название бренда сторонний класс
         CarBrand,
         on_delete=models.CASCADE,
         related_name='cars',
         verbose_name='Brand'
     )
-    model = models.ForeignKey(
+    model = models.ForeignKey(  # название модели сторонний класс
         CarModel,
         on_delete=models.CASCADE,
         related_name='cars_model',
         verbose_name='Model'
     )
-    act = models.ForeignKey(
+    act = models.ForeignKey(  # информация о страховке, сторонний класс
         ACT,
         on_delete=models.CASCADE,
         related_name='car_act',
         verbose_name='Car'
     )
-    first_class = models.ForeignKey(
+    first_class = models.ForeignKey( # информация о страховке, сторонний класс
         FirstClass,
         on_delete=models.CASCADE,
         related_name='car_first_class',
         verbose_name='Car'
     )
-    second_class = models.ForeignKey(
+    second_class = models.ForeignKey(  # информация о страховке, сторонний класс
         SecondClass,
         on_delete=models.CASCADE,
         related_name='car_second_class',
         verbose_name='Car'
     )
-    tax = models.ForeignKey(
+    tax = models.ForeignKey(  #  информация о страховке, сторонний класс
         Tax,
         on_delete=models.CASCADE,
         related_name='car_tax',
         verbose_name='Car'
     )
-    engine = models.OneToOneField(
+    bluebook = models.ForeignKey(  # информация о страховке, сторонний класс
+        Bluebook,
+        on_delete=models.CASCADE,
+        related_name='car_bluebook',
+        verbose_name='Car'
+    )
+    engine = models.OneToOneField(  # информация о двигателе, сторонний класс
         Engine,
         on_delete=models.CASCADE,
         related_name='car_engine',
         verbose_name='Engine'
     )
-    chassis = models.OneToOneField(
+    chassis = models.OneToOneField(  # информация о шасси, сторонний класс
         Chassis,
         on_delete=models.CASCADE,
         related_name='car_chassis',
         verbose_name='Chassis'
     )
-    music = models.OneToOneField(
+    music = models.OneToOneField(  # информация о муз.возможностях, сторонний класс
         Music,
         on_delete=models.CASCADE,
         related_name='car_music',
         verbose_name='Music'
     )
-    other = models.OneToOneField(
+    other = models.OneToOneField(  # информация о другом сторонний класс
         Other,
         on_delete=models.CASCADE,
         related_name='car_other',
         verbose_name='Other Features'
     )
-    photos = models.ManyToManyField(
+    photos = models.ManyToManyField(  # фотографии автомобилей сторонний класс
         Photo,
         related_name='cars_photos',
         verbose_name='Photos',
         blank=True
     )
-    problems = models.ManyToManyField(
+    problems = models.ManyToManyField(  # проблемы с автомобилем сторонний класс 
         Problem,
         related_name='cars_problems',
         verbose_name='Problems',
         blank=True
     )
-    number = models.CharField(
+    number = models.CharField( # гос номер
         max_length=64,
     )
-    year_manufactured = models.PositiveIntegerField(
+    year_manufactured = models.PositiveIntegerField(  # год выпуска авто
         validators=[
             validate_manufactured_year
         ],
         verbose_name='Year Manufactured'
     )
-    body_type = models.CharField(
+    body_type = models.CharField(  #  тип кузова 
         max_length=128,
         choices=BODY_TYPE_CHOICES
     )
-    deposit = models.PositiveIntegerField()
-    color = models.CharField(
+    deposit = models.PositiveIntegerField()  # депозит для авто
+    color = models.CharField(  # цвет автомобиля
         max_length=64,
         choices=COLOR_CHOICES
     )
-    created_at = models.DateTimeField(
+    created_at = models.DateTimeField(  # дата и время создания в бд данного авто
         auto_now_add=True,
         verbose_name='Created At'
     )
-    updated_at = models.DateTimeField(
+    updated_at = models.DateTimeField(  #  дата и время изменения записи об авто в бд
         auto_now=True,
         verbose_name='Updated At'
     )
@@ -388,27 +410,26 @@ class Car(models.Model):
         return f'{self.brand.name} {self.model.name}'
 
 
-
 class Price(models.Model):
-    pick_season = models.DecimalField(
+    pick_season = models.DecimalField(  # цена в пиковом сезоне
         max_digits=10,
         decimal_places=2,
         null=True,
         blank=True
         )
-    high_season = models.DecimalField(
+    high_season = models.DecimalField(  # цена в высоком сезоне
         max_digits=10,
         decimal_places=2,
         null=True,
         blank=True
         )
-    low_season = models.DecimalField(
+    low_season = models.DecimalField(  # цена в низком сезоне
         max_digits=10,
         decimal_places=2,
         null=True,
         blank=True
         )
-    currency = models.CharField(
+    currency = models.CharField(  # тип валюты? нужен ли и как испольлзовать вопрос
         max_length=10,
         null=True,
         choices=CURRENCY_CHOICES,
@@ -416,32 +437,31 @@ class Price(models.Model):
 
 
 class Application(models.Model):
-    num = models.IntegerField(
+    num = models.IntegerField(  # номер заявки
         unique=True,
         null=True,
         blank=True
     )
-    aggregator = models.CharField(
+    aggregator = models.CharField(  # с какого агрегатора заявка
         max_length=128,
         choices=AGGREGATOR_CHOICES
     )
-    date = models.DateField()
-    auto = models.ForeignKey(
+    auto = models.ForeignKey(  # какое авто фигурирует в заявке
         'Car',
         on_delete=models.CASCADE,
         related_name='applications',
         verbose_name='Car'
     )
-    location_delivery = models.CharField(
+    location_delivery = models.CharField(  # локация доставки авто
         max_length=256
     )
-    location_return = models.CharField(
+    location_return = models.CharField(  # локация возврата авто
         max_length=256
     )
-    name = models.CharField(
+    client_name = models.CharField(  # имя клиента
         max_length=256
     )
-    birthdate = models.DateField(
+    birthdate = models.DateField(  # дата рождения клиента 
         blank=True,
         null=True
     )
